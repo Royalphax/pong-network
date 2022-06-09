f7ad83ef89527dc92e9f29f4f92c13be6b862849
Royalphax <therenceforot@orange.fr>
2022-06-09 08:55:57 +0200

    Ajout de la class Socket

---
+++ src/client/SocketManager.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Therence on 08/06/2022.
+//
+
+#include "SocketManager.h"
+
+void SocketManager::connect(char *ip, GUIState &state) {
+    // TODO: à compléter. Fonction lorsque le client essaye d'établir la connexion avec le serveur
+
+}
---
+++ src/client/SocketManager.h
@@ -0,0 +1,24 @@
+//
+// Created by Therence on 08/06/2022.
+//
+
+#ifndef MAIN_CPP_SOCKETMANAGER_H
+#define MAIN_CPP_SOCKETMANAGER_H
+
+#include "GUIState.h"
+
+class SocketManager {
+
+private:
+    // TODO: définir les variables privées nécessaires
+
+public:
+    // TODO: définir les variables publics nécessaires
+
+    void connect(char* ip, GUIState &state); // Méthode appellée lorsque le joueur entre dans le menu de connexion au serveur
+
+    // TODO: rajouter les fonctions nécessaires pour se connecter/gérer la connexion
+};
+
+
+#endif //MAIN_CPP_SOCKETMANAGER_H
